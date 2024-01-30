3d9bfda319f3e700142923d6522a696de51b8365
Ect0spasm <143848554+Ect0spasm@users.noreply.github.com>
2024-01-30 14:05:00 -0800

    Arrays

---
+++ stetson-g-arrays.js
@@ -0,0 +1,83 @@
+/*
+Arrays [0]
+*/
+
+//Organizing and storing ordered information; is foundational to programming.
+
+
+//Lists can organize real life data
+/*
+How to make it:
+1. Make a bed
+2. Doing things
+3. You get it
+*/
+
+//Now it's time to write a list in Javascript as an array
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn Kubernetes'];
+let howTo = ['Make a bed', 'Doing things', 'You get it'];
+
+//Arrays are Javascript's way of making lists.
+/*
+Arrays can store any data types (including strings, numbers, and booleans)
+Like lists, they are ordered
+*/
+
+//Creating an array
+/*
+One way is to use an array literal.
+It creates am array by wrapping items in [square brackets]
+Arrays can store any data type
+
+Each element within an array is called an element.
+Each element is seperated by comma
+*/
+
+//practice
+const hobbies = ['Gaming', 'guitar', 'art'];
+console.log(hobbies);
+
+
+
+//Accessing Elements
+/*
+Every element has a numbered position called an index.
+You can access indv. items by using theit index, its like referencing
+ an item based on its position (or number) 
+ Javascript arrays are zero-indexed. meaning they start counting from 0 instead of 1.
+*/
+
+
+
+//Practiuce
+    listItem = [0];
+    console.log(listItem);
+    listItem = [2];
+    listItem = [3];
+
+
+
+
+//Updating Elements
+/*
+    We learned how to access elements inside an array from the last exercise
+    Once we have acces to an element in an array, we can update its value 
+*/
+let seasons = ['Winter', 'Spring', 'Summer', 'Fall'];
+seasons[3] = 'Autumn';
+console.log(seasons);
+
+
+//Practice
+/*
+    change each value in newYearsResolutions array without directly editing the array 
+    at the top of the script to what I want to accopmplish
+    Then console.log to check the modified array
+*/
+
+
+
+//
+/*
+
+*/
